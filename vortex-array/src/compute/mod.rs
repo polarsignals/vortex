19220f853d0a1f9313d7bedb2fe8fe7e85bb6055
Joe Isaacs <joe.isaacs@live.co.uk>
2025-06-25 09:23:56 +0100

    bug[vortex-array]: take compare nullable bugs (#3628)
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

--- vortex-array/src/compute/mod.rs
+++ vortex-array/src/compute/mod.rs
@@ -113,10 +113,14 @@ impl ComputeFn {
 
         if output.dtype() != &expected_dtype {
             vortex_bail!(
-                "Internal error: compute function {} returned a result of type {} but expected {}",
+                "Internal error: compute function {} returned a result of type {} but expected {}\n{}",
                 self.id,
                 output.dtype(),
                 &expected_dtype,
+                args.inputs
+                    .iter()
+                    .filter_map(|input| input.array())
+                    .format_with(",", |array, f| f(&array.tree_display()))
             );
         }
         if output.len() != expected_len {
